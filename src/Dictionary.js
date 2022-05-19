200a10a555e63500dabc055efe5e62a6baf6b288
eirawyn84 <97123947+eirawyn84@users.noreply.github.com>
2022-05-19 13:29:32 +0100

    Create results and meaning components to use data from API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   // https://api.dictionaryapi.dev/api/v2/entries/en/<word>
 
   function handleResponse(response) {
-    console.log(response.data[0]);
+    console.log();
+    setResults(response.data[0]);
   }
 
   function search(event) {
@@ -29,6 +32,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
