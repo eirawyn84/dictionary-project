c95411cb104985f6de0375f706960567618e203a
eirawyn84 <97123947+eirawyn84@users.noreply.github.com>
2022-05-27 18:14:06 +0100

    added Github link

--- src/App.js
+++ src/App.js
@@ -14,7 +14,16 @@ export default function App() {
         </main>
 
         <footer className="App-footer">
-          <small>Coded by Eira Owen</small>
+          <small>
+            This project was Coded by Eira Owen and is{" "}
+            <a href="https://github.com/eirawyn84/dictionary-project">
+              open-sourced on GitHub{" "}
+            </a>
+            and{" "}
+            <a href="https://cheerful-douhua-47e288.netlify.app/">
+              hosted on Netlify{" "}
+            </a>
+          </small>
         </footer>
       </div>
     </div>
