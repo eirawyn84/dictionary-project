79d67c1793f8ae941f68a4bab6601af752ea28d9
eirawyn84 <97123947+eirawyn84@users.noreply.github.com>
2022-06-02 08:00:58 +0100

    Update PixelsApi Url

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -30,7 +30,7 @@ export default function Dictionary(props) {
 
     let pexelsApiKey =
       "563492ad6f917000010000015677a7df6a864afeb633e0e541d29634";
-    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=`;
     let headers = { Authorization: `Bearer ${pexelsApiKey}` };
     axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
